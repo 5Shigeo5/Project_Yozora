95be302c20150e6789f1a2abca394699f41734ce
5Shigeo5 <obico.steven.01@gmail.com>
2026-08-29 15:56:14 +0800

    add: welcome message development

---
+++ src/events/memberAdd.js
@@ -0,0 +1,34 @@
+import {
+    EmbedBuilder
+} from 'discord.js';
+
+export default{
+
+    name: 'guildMemberAdd',
+
+    async execute(member){
+
+        const welcomeChannelId = process.env.DISCORD_WELCOME_CHANNEL_ID;
+        const channel = member.guild.channels.cache.get(welcomeChannelId);
+
+        if(!channel) return;
+
+        const embed = new EmbedBuilder()
+            .setTitle("Welcome to Node 0101")
+            .setDescription(`A new guest has entered the node!\n\n**Welcome to Node 0101 Bar and Cafe <@${member.id}>**\n\n**Please Head to the Rules And Verification to gain full access to the server**`)
+            .setColor("#a6ffd2")
+            .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+            .setTimestamp();
+
+        try{
+            await channel.send({
+                embeds: [embed]
+            });
+        }catch (error){
+            console.error('Failed to send welcome message: ', error);
+        }
+    }
+
+    
+
+}
